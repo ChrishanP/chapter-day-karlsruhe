292c372a23d09d3e39da9970d5cb60c6d8cdd755
Chrishan Perera <chrishan.perera@logmein.com>
2021-06-28 15:13:36 +0200

    Embed tileset in map

--- Session Names.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="Session Names" tilewidth="32" tileheight="32" tilecount="1980" columns="60">
- <image source="session-names.png" width="1920" height="1080"/>
-</tileset>
